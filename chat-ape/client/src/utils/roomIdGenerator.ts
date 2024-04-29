cb72705e52ee0c63605b3050b58a3d7cd7ea46f5
MrSaadMasood <mrsaadmasood1@gmail.com>
2024-04-30 00:24:30 +0500

    chore: correct documentation

--- chat-ape/client/src/utils/roomIdGenerator.ts
+++ chat-ape/client/src/utils/roomIdGenerator.ts
@@ -1,5 +1,5 @@
+// generate a room id so users can connect to this room
 
-    // generate a room id so users can connect to this room
 export function generateRoomId(userId : string, friendId : string) {
         const sortedArray = [userId, friendId].sort();
         return `room${sortedArray[0]},${sortedArray[1]}`;
