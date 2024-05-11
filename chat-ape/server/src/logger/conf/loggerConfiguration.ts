6580c178ea9a6a9cefcd32cfc50457e667ef50e3
MrSaadMasood <mrsaadmasood1@gmail.com>
2024-05-11 20:57:25 +0500

    fix: add logs files deletion time

--- chat-ape/server/src/logger/conf/loggerConfiguration.ts
+++ chat-ape/server/src/logger/conf/loggerConfiguration.ts
@@ -1,56 +1,63 @@
-import winston from "winston"
-import path from "path"
-import "winston-daily-rotate-file"
-import env from "../../zodSchema/envSchema.js"
+import winston from "winston";
+import path from "path";
+import "winston-daily-rotate-file";
+import env from "../../zodSchema/envSchema.js";
 
-const { LOGGER_LEVEL = "info" } = env
-const currentWorkingDirectory = process.cwd()
+const { LOGGER_LEVEL = "info" } = env;
+const currentWorkingDirectory = process.cwd();
 
-const { combine, json, timestamp, errors } = winston.format
-const loggingFormat = combine(errors({ stack : true }), timestamp(), json())
+const { combine, json, timestamp, errors } = winston.format;
+const loggingFormat = combine(errors({ stack: true }), timestamp(), json());
 
-const fileRotateTransport = (type : string) =>  
-    new winston.transports.DailyRotateFile(configBasedOnFileType(type))
-const configBasedOnFileType = (type : string) => {
+const fileRotateTransport = (type: string) =>
+    new winston.transports.DailyRotateFile(configBasedOnFileType(type));
 
-    const logsDirectoryPath = path.join(currentWorkingDirectory, "/src/logger/logs/")
+const configBasedOnFileType = (type: string) => {
+    const logsDirectoryPath = path.join(
+        currentWorkingDirectory,
+        "/src/logger/logs/",
+    );
     return {
-        datePattern : "YYYY-MM-DD",
-        filename : `${type}-%DATE%.log`,
-        dirname : path.join(logsDirectoryPath, type),
-        level : type.includes("error") ? "error" : LOGGER_LEVEL
-    }
-}
-
+        datePattern: "YYYY-MM-DD",
+        maxFiles: "3d",
+        filename: `${type}-%DATE%.log`,
+        dirname: path.join(logsDirectoryPath, type),
+        level: type.includes("error") ? "error" : LOGGER_LEVEL,
+    };
+};
 
 const logger = winston.createLogger({
-    level : LOGGER_LEVEL,
-    format : loggingFormat,
-    transports : [
-        fileRotateTransport("combined"),
-        fileRotateTransport("error"),
+    level: LOGGER_LEVEL,
+    format: loggingFormat,
+    transports: [fileRotateTransport("combined"), fileRotateTransport("error")],
+    exceptionHandlers: [
+        new winston.transports.File({
+            dirname: path.join(
+                currentWorkingDirectory,
+                "/src/logger/logs/exceptions/",
+            ),
+            filename: "exceptions.log",
+        }),
     ],
-    exceptionHandlers : [ new winston.transports.File({
-        dirname : path.join(currentWorkingDirectory, "/src/logger/logs/exceptions/"),
-        filename : "exceptions.log"
-    })],
-    rejectionHandlers : [ new winston.transports.File({
-        dirname : path.join(currentWorkingDirectory, "/src/logger/logs/rejections/"),
-        filename : "rejections.log"
-    })]
-})
+    rejectionHandlers: [
+        new winston.transports.File({
+            dirname: path.join(
+                currentWorkingDirectory,
+                "/src/logger/logs/rejections/",
+            ),
+            filename: "rejections.log",
+        }),
+    ],
+});
 
 const serverLogger = winston.createLogger({
-    level : LOGGER_LEVEL,
-    format : loggingFormat,
-    transports : [
+    level: LOGGER_LEVEL,
+    format: loggingFormat,
+    transports: [
         new winston.transports.Console(),
-        fileRotateTransport("server-error")
-    ]
-})
+        fileRotateTransport("server-error"),
+    ],
+});
 
+export { logger, serverLogger };
 
-export {
-    logger,
-    serverLogger
-}
