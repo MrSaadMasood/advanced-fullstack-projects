cb671c8cf1368c9185de29970ffec6c683327440
MrSaadMasood <mrsaadmasood1@gmail.com>
2024-09-13 03:12:06 +0500

    chore

--- chat-ape/server/dist/src/middlewares/imageHandler.js
+++
@@ -1,6 +0,0 @@
-export const imageHandlerMiddleware = (propertyName) => {
-    return (req, _res, next) => {
-        Object.defineProperty(req, propertyName, { value: true });
-        next();
-    };
-};
--- chat-ape/server/dist/src/utils/oAuth2Client.js
+++
@@ -1,5 +0,0 @@
-import { OAuth2Client } from "google-auth-library";
-import env from "../zodSchema/envSchema.js";
-const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = env;
-const oAuth2Client = new OAuth2Client(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, "postmessage");
-export default oAuth2Client;
